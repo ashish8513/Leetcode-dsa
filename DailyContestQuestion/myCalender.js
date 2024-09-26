5525d0ba4a35ceb7062b33a41e6ef6e0569f067b
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2024-09-26 11:41:26 +0530

    added by me

---
+++ DailyContestQuestion/myCalender.js
@@ -0,0 +1,36 @@
+// You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a double booking.
+
+// A double booking happens when two events have some non-empty intersection (i.e., some moment is common to both events.).
+
+// The event can be represented as a pair of integers start and end that represents a booking on the half-open interval [start, end), the range of real numbers x such that start <= x < end.
+
+// Implement the MyCalendar class:
+
+// MyCalendar() Initializes the calendar object.
+// boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a double booking. Otherwise, return false and do not add the event to the calendar.
+ 
+var MyCalendar = function () {
+    this.calendar = [];
+};
+
+/**
+ * @param {number} start
+ * @param {number} end
+ * @return {boolean}
+ */
+MyCalendar.prototype.book = function (start, end) {
+    for (const item of this.calendar) {
+        if (end <= item[0] || item[1] <= start) {
+            continue;
+        }
+        return false;
+    }
+    this.calendar.push([start, end]);
+    return true;
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * var obj = new MyCalendar()
+ * var param_1 = obj.book(start,end)
+ */
