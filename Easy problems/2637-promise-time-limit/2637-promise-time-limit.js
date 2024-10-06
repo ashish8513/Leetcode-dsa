8bf073098946af9936b2dcadcbc2d3e5bb223663
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2024-10-06 18:51:26 +0530

    added some data

--- Easy problems/2637-promise-time-limit/2637-promise-time-limit.js
+++ Easy problems/2637-promise-time-limit/2637-promise-time-limit.js
@@ -19,7 +19,3 @@ var timeLimit = function(fn, t) {
     }
 };
 
-/**
- * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
- * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
- */
