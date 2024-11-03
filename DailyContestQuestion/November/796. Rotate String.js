0158d4e76980e963c1b0f0f2d5f3ec41f99d528d
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2024-11-03 11:06:24 +0530

    added

---
+++ DailyContestQuestion/November/796. Rotate String.js
@@ -0,0 +1,18 @@
+// Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+// A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+// For example, if s = "abcde", then it will be "bcdea" after one shift.
+
+
+
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function(s, goal) {
+    if(s.length !== goal.length) return false;
+   let str = s.concat(s);
+   return str.includes(goal);
+};
