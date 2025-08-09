32fe38611cca78f93258ab56a2441841bafed843
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2025-08-10 01:29:47 +0530

    adde

--- Dsa/1.js
+++ Dsa/1.js
@@ -45,8 +45,8 @@ console.log("Max:", FindMaxandMin([1, 2, 3, 4, 9]));
 // count Vowels in a string 
 
 function countVowels(str) {
-    const vowels="aeiou";
-    let count =0;
+    const vowels = "aeiou";
+    let count = 0;
     for (let i = 0; i < str.length; i++) {
         if (vowels.includes(str[i].toLowerCase())) {
             count++;
@@ -55,4 +55,43 @@ function countVowels(str) {
     return count;
 }
 
-console.log("Count of Vowels:", countVowels("Ashish Prabhakar"));   
+console.log("Count of Vowels:", countVowels("Ashish Prabhakar"));
+
+// Sum of array elements
+
+function sumOfArray(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum;
+
+}
+
+console.log("Sum of Array:", sumOfArray([1, 2, 3, 4, 5]));
+
+function sumOfArray(arr) {
+    return arr.reduce((acc, num) => acc + num, 0);
+}
+console.log("Sum of :", sumOfArray([1, 2, 3, 4, 3]));
+
+// Factorial of a <number 
+function Factorial(num) {
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+        result *= i;
+    }
+    return result;
+}
+console.log("Factorial of 5:", Factorial(5));
+
+//  Fibonacci series first N terms
+
+function fibonacci(n) {
+    let series = [0, 1];
+    for (let i = 2; i < n; i++) {
+        series.push(series[i - 1] + series[i - 2])
+    }
+    return series.slice(0, n);
+}
+console.log("Fibonacci series:", fibonacci(10));
