b80e219353ce9d80a9876c43243deed9b2ac7d12
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2025-08-10 01:21:36 +0530

    adde

--- Dsa/1.js
+++ Dsa/1.js
@@ -12,10 +12,47 @@ console.log("level", reverseNumber(1234));
 
 //reverse string
 
-function isPlaindrom(str){
-    const rev=str.split('').reverse().join('')
+function isPlaindrom(str) {
+    const rev = str.split('').reverse().join('')
     return str === rev;
 }
 
 console.log("level", isPlaindrom("level"));
 
+
+// Find max & min in an array
+// method 1 approach 
+// function findMaxMin(arr) {
+//     const max = Math.max(...arr);
+//     return max;
+// }
+
+// console.log("Max:", findMaxMin([1, 2, 3, 4, 5]));
+
+// method 2 approach
+function FindMaxandMin(arr) {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+console.log("Max:", FindMaxandMin([1, 2, 3, 4, 9]));
+
+
+// count Vowels in a string 
+
+function countVowels(str) {
+    const vowels="aeiou";
+    let count =0;
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i].toLowerCase())) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log("Count of Vowels:", countVowels("Ashish Prabhakar"));   
