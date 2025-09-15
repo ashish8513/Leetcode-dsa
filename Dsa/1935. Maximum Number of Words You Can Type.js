c6583b060c7f32d5c0a8520233306234278a52db
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2025-09-16 00:32:22 +0530

    added

---
+++ Dsa/1935. Maximum Number of Words You Can Type.js
@@ -0,0 +1,14 @@
+// There is a malfunctioning keyboard where some letter keys do not work. All other keys on the keyboard work properly.
+
+// Given a string text of words separated by a single space (no leading or trailing spaces) and a string brokenLetters of all distinct letter keys that are broken, return the number of words in text you can fully type using this keyboard.
+
+/**
+ * @param {string} text
+ * @param {string} brokenLetters
+ * @return {number}
+ */
+var canBeTypedWords = function(text, brokenLetters) {
+     const brokenSet = new Set(brokenLetters);
+    const words = text.split(" ");
+    return words.filter(word =>![...word].some(char => brokenSet.has(char))).length;
+};
