71e5838e284f3d0dd2a61dca256c45b5849673c3
Ashish Prabhakar <141730191+ashish8513@users.noreply.github.com>
2025-12-20 21:56:59 +0530

    Add function to count unsorted columns in strings
    Implement function to count columns to delete for sorted order.

---
+++ Dsa/944. Delete Columns to Make Sorted.js
@@ -0,0 +1,33 @@
+// You are given an array of n strings strs, all of the same length.
+
+// The strings can be arranged such that there is one on each line, making a grid.
+
+// For example, strs = ["abc", "bce", "cae"] can be arranged as follows:
+// abc
+// bce
+// cae
+// You want to delete the columns that are not sorted lexicographically. In the above example (0-indexed), columns 0 ('a', 'b', 'c') and 2 ('c', 'e', 'e') are sorted, while column 1 ('b', 'c', 'a') is not, so you would delete column 1.
+
+// Return the number of columns that you will delete.
+
+
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+var minDeletionSize = function(strs) {
+  let rows = strs.length;
+  let cols = strs[0].length;
+  let deleteCount = 0;
+
+  for (let col = 0; col < cols; col++) {
+    for (let row = 1; row < rows; row++) {
+      if (strs[row][col] < strs[row - 1][col]) {
+        deleteCount++;
+        break; 
+      }
+    }
+  }
+
+  return deleteCount;
+};
