c1ced8bcd9c6167d9a7a16a54b0c318f49f99ec2
Ashish Prabhakar <141730191+ashish8513@users.noreply.github.com>
2026-01-01 18:17:10 +0530

    Create 66. Plus One.py

---
+++ Dsa/66. Plus One.py
@@ -0,0 +1,16 @@
+# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+# Increment the large integer by one and return the resulting array of digits.
+
+
+class Solution:
+    def plusOne(self, digits):
+        n = len(digits)
+
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+
+        return [1] + digits
