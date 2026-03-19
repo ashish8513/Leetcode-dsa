001b49b9bf0e7b7c181a7809cc39b7a483e49d18
Ashish Prabhakar <141730191+ashish8513@users.noreply.github.com>
2026-03-19 11:01:20 +0530

    Create count-submatrices-with-equal-frequency-of-x-and-y.cpp

---
+++ count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -0,0 +1,29 @@
+// Given a 2D character matrix grid, where grid[i][j] is either 'X', 'Y', or '.', return the number of submatrices that contain:
+
+// grid[0][0]
+// an equal frequency of 'X' and 'Y'.
+// at least one 'X'.
+
+class Solution {
+public:
+    int numberOfSubmatrices(vector<vector<char>>& grid) {
+        int rows = grid.size(), cols = grid[0].size();
+        vector<int> sumX(cols, 0);
+        vector<int> sumY(cols, 0);
+        int res = 0;
+
+        for (int i = 0; i < rows; i++) {
+            int rx = 0, ry = 0;
+
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 'X') rx++;
+                else if (grid[i][j] == 'Y') ry++;
+                sumX[j] += rx;
+                sumY[j] += ry;
+                if (sumX[j] > 0 && sumX[j] == sumY[j]) res++;
+            }
+        }
+
+        return res;
+    }
+};
