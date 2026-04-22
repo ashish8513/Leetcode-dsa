042b5b68429b238c4fe6c5eb238f4d5f5da09a49
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2026-04-22 10:36:57 +0530

    intial commit

---
+++ 01_Arrays/two_sum.cpp
@@ -0,0 +1,6 @@
+// Problem: Two Sum
+// Platform: LeetCode
+// Approach: Hash Map
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
---
+++ Basics/input.output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Enter your age:" << endl;
+    cin >> age;
+    cout << "your age is : " << age << endl;
+    return 0;
+}
