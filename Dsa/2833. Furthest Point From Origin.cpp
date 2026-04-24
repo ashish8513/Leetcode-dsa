ce73a32bbbcb70023162728ff6f55d726c826547
Ashish Prabhakar <141730191+ashish8513@users.noreply.github.com>
2026-04-24 12:14:20 +0530

    Create 2833. Furthest Point From Origin.cpp

---
+++ Dsa/2833. Furthest Point From Origin.cpp
@@ -0,0 +1,24 @@
+// You are given a string moves of length n consisting only of characters 'L', 'R', and '_'. The string represents your movement on a number line starting from the origin 0.
+
+// In the ith move, you can choose one of the following directions:
+
+// move to the left if moves[i] = 'L' or moves[i] = '_'
+// move to the right if moves[i] = 'R' or moves[i] = '_'
+// Return the distance from the origin of the furthest point you can get to after n moves.
+
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int L = 0, R = 0, blank = 0;
+        
+        for(char c : moves) {
+            if(c == 'L') L++;
+            else if(c == 'R') R++;
+            else blank++;
+        }
+        
+        int pos = R - L;
+        
+        return max(abs(pos + blank), abs(pos - blank));
+    }
+};
