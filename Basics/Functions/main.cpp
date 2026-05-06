5e3185259688dc68d79893040bb5ec3b523bdeca
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2026-05-06 23:38:07 +0530

    functions

---
+++ Basics/Functions/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+// return type
+// function name
+// input type
+int ashish(int n)
+{
+    int result = 2 * n;
+    return result;
+}
+int main()
+{
+    // Write C++ code here
+    int m = 10;
+    int result = ashish(m);
+    cout << result << endl;
+    return 0;
+}
