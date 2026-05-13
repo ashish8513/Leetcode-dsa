f07bfcead68643493b6d72ab2b234b1554bf3597
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2026-05-14 01:21:16 +0530

    pattern recoginstation

---
+++ Basics/Star Patterns/pattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 6;
+    for (int i = 3; i >= 1; i--)
+    {
+        // spaces
+        for (int s = 3; s > i; s--)
+        {
+            cout << "-";
+        }
+        // numbers
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num << "";
+            num--;
+        }
+        cout << endl;
+    }
+    return 0;
+}
