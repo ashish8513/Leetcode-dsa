be0567fb6dda759f8a521941802c84f9866c6ff1
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2026-05-15 01:23:01 +0530

    if array is good

---
+++ Dsa/2784. Check if Array is Good.cpp
@@ -0,0 +1,27 @@
+// You are given an integer array nums. We consider an array good if it is a permutation of an array base[n].
+
+// base[n] = [1, 2, ..., n - 1, n, n] (in other words, it is an array of length n + 1 which contains 1 to n - 1 exactly once, plus two occurrences of n). For example, base[1] = [1, 1] and base[3] = [1, 2, 3, 3].
+
+// Return true if the given array is good, otherwise return false.
+
+// Note: A permutation of integers represents an arrangement of these numbers.
+
+
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int n = nums.size() - 1;
+
+        sort(nums.begin(), nums.end());
+
+        // Check numbers from 1 to n-1
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] != i + 1) {
+                return false;
+            }
+        }
+
+        // Last two elements should be n
+        return nums[n - 1] == n && nums[n] == n;
+    }
+};
