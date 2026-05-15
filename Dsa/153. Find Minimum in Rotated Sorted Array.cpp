21134f84da39556844f4d35f7b79ca721b8eb1cf
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2026-05-16 00:57:15 +0530

    vector database

---
+++ Dsa/153. Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,32 @@
+// Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+// [4,5,6,7,0,1,2] if it was rotated 4 times.
+// [0,1,2,4,5,6,7] if it was rotated 7 times.
+// Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+// Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+// You must write an algorithm that runs in O(log n) time.
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            // Minimum right side mein hai
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            }
+            // Minimum left side ya mid par hai
+            else {
+                high = mid;
+            }
+        }
+
+        return nums[low];
+    }
+};
