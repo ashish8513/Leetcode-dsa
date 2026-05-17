df6e7b7a29ab292178b5a7e4fc798154437211cb
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2026-05-17 14:47:20 +0530

    jump game

--- 01_Arrays/Easy/Sliding Window/1306. Jump Game III.cpp
+++ 01_Arrays/Easy/Sliding Window/1306. Jump Game III.cpp
@@ -1,7 +1,9 @@
 // Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach any index with value 0.
 
 // Notice that you can not jump outside of the array at any time.
-
+#include <iostream>
+#include <vector>
+using namespace std;
 class Solution {
 public:
 
