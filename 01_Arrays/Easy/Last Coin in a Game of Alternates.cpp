5c6290b280abf5e31b1ea3ea7794b8c7d31f4067
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2026-05-24 01:00:03 +0530

    added

---
+++ 01_Arrays/Easy/Last Coin in a Game of Alternates.cpp
@@ -0,0 +1,23 @@
+// Given an array integer arr[] , representing the values of coins arranged in a row.
+
+// Two players play a game by picking coins alternately.
+// At each turn, a player can pick a coin from either the beginning or the end of the array. Both players follow a greedy strategy, i.e., they always pick the coin with the maximum value among the two available ends.
+// The game continues until only one coin remains.
+// Find the value of the last remaining coin.
+
+class Solution {
+  public:
+    int coin(vector<int>& arr) {
+        // code here
+       int i =0 ;
+       int j= arr.size()-1;
+       while(i<j){
+           if(arr[i]>arr[j]){
+               i++;
+           }else{
+               j--;
+           }
+       }
+       return arr[i];
+    }
+};
