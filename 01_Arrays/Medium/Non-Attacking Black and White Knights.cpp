6cfd6e36d509ba7fe201e1b37fcaef23a8d23538
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2026-06-06 12:46:53 +0530

    fix error on the non attacking black and white knights

--- 01_Arrays/Medium/Non-Attacking Black and White Knights.cpp
+++ 01_Arrays/Medium/Non-Attacking Black and White Knights.cpp
@@ -6,3 +6,23 @@
 // A knight can move two squares horizontally and one square vertically (L shaped), or two squares vertically and one square horizontally (L shaped).
 // The knights attack each other if one can reach the other in one move.
 
+class Solution {
+public:
+    long long numOfWays(int n, int m) {
+        long long cells = 1LL * n * m;
+
+        long long total = cells * (cells - 1);
+
+        long long attacking = 0;
+
+        if (n >= 2 && m >= 3)
+            attacking += 1LL * (n - 1) * (m - 2);
+
+        if (n >= 3 && m >= 2)
+            attacking += 1LL * (n - 2) * (m - 1);
+
+        attacking *= 4;
+
+        return total - attacking;
+    }
+};
