e309e2d405db0fb4d071a796b026c3cfb1142a68
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2026-06-09 13:16:33 +0530

    maximum sub array

---
+++ 01_Arrays/3689. Maximum Total Subarray Value I.cpp
@@ -0,0 +1,22 @@
+// You are given an integer array nums of length n and an integer k.
+
+// You need to choose exactly k non-empty subarrays nums[l..r] of nums. Subarrays may overlap, and the exact same subarray (same l and r) can be chosen more than once.
+
+// The value of a subarray nums[l..r] is defined as: max(nums[l..r]) - min(nums[l..r]).
+
+// The total value is the sum of the values of all chosen subarrays.
+
+// Return the maximum possible total value you can achieve.
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+ class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        long long mx = *max_element(nums.begin(), nums.end());
+        long long mn = *min_element(nums.begin(), nums.end());
+
+        return 1LL * k * (mx - mn);
+    }
+}; 
