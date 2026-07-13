df0b03172e6ef8a44257ccb4800971ad0cb566dc
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2026-07-13 09:44:06 +0530

    Rotate array

---
+++ Basics/Rotate Array.cpp
@@ -0,0 +1,8 @@
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+    }
+};
