e7501e36743297c9a2e4baeaf9c4e1c8d1e4b820
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2026-08-01 23:31:16 +0530

    added feat leetcode

---
+++ LeetCode/(LeetCode 486).cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>(n));
+
+        for (int i = 0; i < n; i++)
+            dp[i][i] = nums[i];
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+
+                dp[i][j] = max(
+                    nums[i] - dp[i + 1][j],
+                    nums[j] - dp[i][j - 1]
+                );
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
+    }
+}; 
