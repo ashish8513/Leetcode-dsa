b2c0a58f77bf308c788a7136d8cfd39adfb7c3b4
Ashish Prabhakar <141730191+ashish8513@users.noreply.github.com>
2026-08-20 10:48:07 +0530

    Create 3069. Distribute Elements Into Two Arrays I.cpp

---
+++ LeetCode/3069. Distribute Elements Into Two Arrays I.cpp
@@ -0,0 +1,37 @@
+// You are given a 1-indexed array of distinct integers nums of length n.
+
+// You need to distribute all the elements of nums between two arrays arr1 and arr2 using n operations. In the first operation, append nums[1] to arr1. In the second operation, append nums[2] to arr2. Afterwards, in the ith operation:
+
+// If the last element of arr1 is greater than the last element of arr2, append nums[i] to arr1. Otherwise, append nums[i] to arr2.
+// The array result is formed by concatenating the arrays arr1 and arr2. For example, if arr1 == [1,2,3] and arr2 == [4,5,6], then result = [1,2,3,4,5,6].
+
+// Return the array result.
+
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> arr1, arr2;
+
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            if (arr1.back() > arr2.back()) {
+                arr1.push_back(nums[i]);
+            } else {
+                arr2.push_back(nums[i]);
+            }
+        }
+
+        // result = arr1 + arr2
+        vector<int> result;
+
+        for (int x : arr1)
+            result.push_back(x);
+
+        for (int x : arr2)
+            result.push_back(x);
+
+        return result;
+    }
+};
